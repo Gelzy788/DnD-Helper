753d41d2c0becd3ba7834cb7f4a89d9cd188dd3d
Gelzy <negm.ali@mail.ru>
2024-11-11 00:25:31 +0300

    a complete authorization system has been implemented, the interface of the window with user information has been slightly changed (a button to log out of the account has been added) a complete account protection system using jwt tokens has been added

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class MainWindow(QMainWindow):
         super().__init__()
         loadUi('ui_files\\main_screen.ui', self)
 
+        try:
+            with open('tokens.txt', 'r') as f:
+                data = f.readlines()
+                self.access_token = data[0].strip()
+                self.refresh_token = data[1].strip()
+        except Exception as e:
+            self.access_token = ''
+            self.refresh_token = ''
+
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
@@ -56,6 +65,8 @@ class MainWindow(QMainWindow):
 
         self.profile_screen.main_screen_btn.clicked.connect(
             self.switch_to_main_screen)
+        self.profile_screen.logout_btn.clicked.connect(
+            self.logout)
 
     def switch_to_main_screen(self):
         self.stacked_widget.setCurrentWidget(self.main_screen)
@@ -77,6 +88,17 @@ class MainWindow(QMainWindow):
 
     def switch_to_login_screen(self):
         self.stacked_widget.setCurrentWidget(self.login_screen)
+
+    @staticmethod
+    def token_required(func):
+        def wrapper(self, *args, **kwargs):
+            # Проверяем, истекает ли токен
+            if not self.is_token_expiring_soon():
+                print("Токен истекает, обновляем...")
+                self.refresh_access_token()
+            # Выполняем основную функцию
+            return func(self, **kwargs)
+        return wrapper
 
     def register(self):
         email = self.registration_screen.email_le.text()
@@ -102,27 +124,92 @@ class MainWindow(QMainWindow):
 
         if response.status_code == 200:
             self.login_screen.login_res.setText('login successful')
-            self.token = response.json().get('token')
-            with open('token.txt', 'w') as f:
-                f.write(self.token)
+            self.access_token = response.json().get('access_token')
+            self.refresh_token = response.json().get('refresh_token')
+            with open('tokens.txt', 'w') as f:
+                f.write(self.access_token)
+                f.write('\n')
+                f.write(self.refresh_token)
             print('login successful')
         else:
             self.login_screen.login_res.setText('login failed')
             print('login failed')
 
+    @token_required
     def profile_data(self):
-        if hasattr(self, 'token'):
-            # print(self.token, 'token')
-            response = requests.get(
-                f'http://{IP_ADDRESS}:{PORT}/profile', headers={'Authorization': f'Bearer {self.token}'})
-            data = response.json()
-            self.stacked_widget.setCurrentWidget(self.profile_screen)
-            self.profile_screen.user_id_label.setText(str(data['id']))
-            self.profile_screen.username_label.setText(data['username'])
-            self.profile_screen.email_label.setText(data['email'])
-            self.profile_screen.password_label.setText(data['password'])
+        if self.access_token != '':
+            if hasattr(self, 'access_token'):
+                response = requests.get(
+                    f'http://{IP_ADDRESS}:{PORT}/profile', headers={'Authorization': f'Bearer {self.access_token}'})
+                data = response.json()
+                print(data)
+                print(self.is_token_expiring_soon())
+                self.stacked_widget.setCurrentWidget(self.profile_screen)
+                self.profile_screen.user_id_label.setText(str(data['id']))
+                self.profile_screen.username_label.setText(data['username'])
+                self.profile_screen.email_label.setText(data['email'])
+            else:
+                self.refresh_access_token()
+                if hasattr(self, 'access_token'):
+                    response = requests.get(
+                        f'http://{IP_ADDRESS}:{PORT}/profile', headers={'Authorization': f'Bearer {self.access_token}'})
+                    data = response.json()
+                    print(data)
+                    self.stacked_widget.setCurrentWidget(self.profile_screen)
+                    self.profile_screen.user_id_label.setText(str(data['id']))
+                    self.profile_screen.username_label.setText(
+                        data['username'])
+                    self.profile_screen.email_label.setText(data['email'])
+                else:
+                    print('token not found')
+                    self.switch_to_login_screen()
         else:
-            print('token not found')
+            self.switch_to_login_screen()
+
+    def refresh_access_token(self):
+        response = requests.post(
+            f'http://{IP_ADDRESS}:{PORT}/refresh-token', json={'refresh_token': self.refresh_token})
+
+        if response.status_code == 200:
+            try:
+                json_response = response.json()
+                self.access_token = json_response.get('access_token')
+                with open('tokens.txt', 'w') as f:
+                    f.write(self.access_token)
+                    f.write('\n')
+                    f.write(self.refresh_token)
+            except ValueError:
+                print("Ошибка декодирования JSON: пустой или некорректный ответ")
+        else:
+            try:
+                print(response.json().get('message'))
+            except ValueError:
+                print("Ошибка декодирования JSON: пустой или некорректный ответ")
+            print("Некорректный ответ от сервера или ошибка соединения")
+
+    def is_token_expiring_soon(self):
+        response = requests.post(
+            f'http://{IP_ADDRESS}:{PORT}/access-token-expiration',
+            json={'access_token': self.access_token})
+
+        # Проверяем, что ответ не пустой и имеет статус 200
+        if response.status_code == 200 and response.content:
+            try:
+                return response.json().get('is_valid')
+            except ValueError:
+                print("Ошибка декодирования JSON")
+                return None
+        else:
+            print(response.json().get('message'))
+            print("Некорректный ответ от сервера или ошибка соединения")
+            return None
+
+    def logout(self):
+        self.access_token = ''
+        self.refresh_token = ''
+        with open('tokens.txt', 'w') as f:
+            f.write('')
+        self.switch_to_main_screen()
 
 
 if __name__ == '__main__':
