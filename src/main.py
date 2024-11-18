be619f39f6f353273b7e7b667ad6665d0073c191
Gelzy <negm.ali@mail.ru>
2024-11-18 16:30:00 +0300

    Added the ability to throw dice and delete and edit the questionnaire

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QInputDialog, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QInputDialog, QPushButton, QVBoxLayout, QWidget, QMessageBox
 from PyQt6.uic import loadUi
 from config import *
 import requests
+from random import randint
 
 
 class MainWindow(QMainWindow):
@@ -23,16 +24,24 @@ class MainWindow(QMainWindow):
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
-        self.main_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\main_screen.ui')
-        self.default_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\default_screen.ui')
-        self.login_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\login_screen.ui')
-        self.registration_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\registration_screen.ui')
-        self.profile_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\profile_screen.ui')
-        self.questionnaire_screen = loadUi('C:\Users\Redmi\Documents\DnD-Helper\ui_files\\questionnaire_screen.ui')
+        self.main_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\main_screen.ui')
+        self.default_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\default_screen.ui')
+        self.login_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\login_screen.ui')
+        self.registration_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\registration_screen.ui')
+        self.profile_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\profile_screen.ui')
+        self.questionnaire_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\questionnaire_screen.ui')
         self.create_questionnaire_screen = loadUi(
-            'ui_files\\create_questionnaire_screen.ui')
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\create_questionnaire_screen.ui')
         self.questionnaire_info_screen = loadUi(
-            'ui_files\\questionnaire_info_screen.ui')
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\questionnaire_info_screen.ui')
+        self.questionnaire_edit_screen = loadUi(
+            'C:\\Users\\Redmi\\Documents\\DnD-Helper\\ui_files\\questionnaire_edit_screen.ui')
 
         # Добавляем интерфейсы в QStackedWidget
         self.stacked_widget.addWidget(self.main_screen)
@@ -43,6 +52,7 @@ class MainWindow(QMainWindow):
         self.stacked_widget.addWidget(self.questionnaire_screen)
         self.stacked_widget.addWidget(self.create_questionnaire_screen)
         self.stacked_widget.addWidget(self.questionnaire_info_screen)
+        self.stacked_widget.addWidget(self.questionnaire_edit_screen)
 
         # Подключение кнопок main screen
         self.main_screen.questionnaires_btn.clicked.connect(
@@ -78,6 +88,7 @@ class MainWindow(QMainWindow):
 
         self.main_screen.account_btn.clicked.connect(
             self.profile_data)
+        self.main_screen.throw_dice_btn.clicked.connect(self.throw_dice)
 
         self.profile_screen.main_screen_btn.clicked.connect(
             self.switch_to_main_screen)
@@ -87,6 +98,9 @@ class MainWindow(QMainWindow):
             self.add_friend)
 
         self.questionnaire_info_screen.back_btn.clicked.connect(
+            self.switch_to_questionnaire_screen)
+
+        self.questionnaire_edit_screen.back_btn.clicked.connect(
             self.switch_to_questionnaire_screen)
 
     def switch_to_main_screen(self):
@@ -105,9 +119,16 @@ class MainWindow(QMainWindow):
         self.stacked_widget.setCurrentWidget(self.default_screen)
 
     def switch_to_registration_screen(self):
+        self.registration_screen.reg_res.clear()
+        self.registration_screen.email_le.clear()
+        self.registration_screen.password_le.clear()
+        self.registration_screen.username_le.clear()
         self.stacked_widget.setCurrentWidget(self.registration_screen)
 
     def switch_to_login_screen(self):
+        self.login_screen.login_res.clear()
+        self.login_screen.email_le.clear()
+        self.login_screen.password_le.clear()
         self.stacked_widget.setCurrentWidget(self.login_screen)
 
     @staticmethod
@@ -120,6 +141,19 @@ class MainWindow(QMainWindow):
             # Выполняем основную функцию
             return func(self, **kwargs)
         return wrapper
+
+    def throw_dice(self):
+        dices = ['D2', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20']
+        dice, ok = QInputDialog.getItem(
+            self, 'Dices', 'Select dice:', dices, 0, False)
+        if ok:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Icon.Question)
+
+            msg.setText(f"{randint(1, int(dice.lstrip('D')))}")
+            msg.setWindowTitle("Throwing dice")
+            msg.setStandardButtons(QMessageBox.StandardButton.Ok)
+            msg.exec()
 
     @token_required
     def switch_to_questionnaire_screen(self):
@@ -166,17 +200,91 @@ class MainWindow(QMainWindow):
         screen.exp_label.setText(str(questionnaire['experience']))
         screen.level_label.setText(str(questionnaire['level']))
 
+        try:
+            screen.del_btn.clicked.disconnect()  # Очищаем предыдущие обработчики
+        except TypeError:
+            pass  # Игнорируем ошибку, если обработчик не был подключен
+
+        # Подключаем новый обработчик
+        screen.del_btn.clicked.connect(
+            # Передаем правильный id
+            lambda: self.del_questionnaire(questionnaire['id']))
+        # self.questionnaire_info_screen.del_btn.clicked.connect(
+        # lambda: self.del_questionnaire(questionnaire['id']))
+        self.questionnaire_info_screen.edit_btn.clicked.connect(lambda:
+                                                                self.switch_to_edit_questionnaire_screen(questionnaire))
+
+    def del_questionnaire(self, id):
+        response = requests.post(
+            f'http://{IP_ADDRESS}:{PORT}/del_questionnaire', json={'id': id})
+        if response.status_code == 200:
+            print(response.json().get('message'))
+            self.switch_to_questionnaire_screen()
+        else:
+            print(response.json().get('message'))
+
+    def switch_to_edit_questionnaire_screen(self, questionnaire):
+        self.stacked_widget.setCurrentWidget(self.questionnaire_edit_screen)
+        screen = self.questionnaire_edit_screen
+        screen.name_input.setText(questionnaire['character_name'])
+        screen.class_input.setText(questionnaire['class_name'])
+        screen.species_input.setText(questionnaire['species'])
+        screen.background_input.setText(questionnaire['background'])
+        screen.worldview_input.setText(questionnaire['worldview'])
+        screen.exp_input.setValue(questionnaire['experience'])
+        screen.level_input.setValue(questionnaire['level'])
+        # print('>>', questionnaire['id'])
+        screen.save_btn.blockSignals(True)
+
+        try:
+            screen.save_btn.clicked.disconnect()
+        except TypeError:
+            pass
+
+        # Подключаем новый обработчик
+        screen.save_btn.clicked.connect(
+            lambda: self.edit_questionnaire(questionnaire['id']))
+
+        # Разблокируем сигналы
+        screen.save_btn.blockSignals(False)
+        # screen.save_btn.clicked.disconnect()
+        # screen.save_btn.clicked.connect(
+        #     lambda: self.edit_questionnaire(questionnaire['id']))
+
+    def edit_questionnaire(self, id):
+        screen = self.questionnaire_edit_screen
+        print(id)
+        response = requests.post(
+            f'http://{IP_ADDRESS}:{PORT}/edit_questionnaire', json={'id': id,
+                                                                    'name': screen.name_input.text(),
+                                                                    'class_name': screen.class_input.text(),
+                                                                    'species': screen.species_input.text(),
+                                                                    'background': screen.background_input.text(),
+                                                                    'worldview': screen.worldview_input.text(),
+                                                                    'experience': screen.exp_input.value(),
+                                                                    'level': screen.level_input.value()})
+        if response.status_code == 200:
+            print('Анкета была отредактирована')
+        else:
+            # print("Хуета вышла")
+            print(response.json().get('message'))
+
     def register(self):
         email = self.registration_screen.email_le.text()
         password = self.registration_screen.password_le.text()
         username = self.registration_screen.username_le.text()
 
-        response = requests.post(
-            f'http://{IP_ADDRESS}:{PORT}/registration', json={'email': email, 'password': password, 'username': username})
+        if email != '' and password != '':
+            response = requests.post(
+                f'http://{IP_ADDRESS}:{PORT}/registration', json={'email': email, 'password': password, 'username': username})
 
-        if response.status_code == 200:
-            self.registration_screen.reg_res.setText('registration successful')
-            print('registration successful')
+            if response.status_code == 200:
+                self.registration_screen.reg_res.setText(
+                    'registration successful')
+                print('registration successful')
+            else:
+                self.registration_screen.reg_res.setText('registration failed')
+                print('registration failed')
         else:
             self.registration_screen.reg_res.setText('registration failed')
             print('registration failed')
@@ -308,6 +416,9 @@ class MainWindow(QMainWindow):
         else:
             # print(response.json().get('message'))
             print('ОШИБКА')
+
+    def delete_questionnaire(self):
+        pass
 
 
 if __name__ == '__main__':
